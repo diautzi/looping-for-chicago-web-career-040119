7119f6fdfcfc8357cb41bddbafbbed23dd02d823
Diana Utan <didiutzi@gmail.com>
2020-11-28 16:51:34 +0000

    Automatically backed up by Learn

--- for.rb
+++ for.rb
@@ -1,10 +1,15 @@
 
-def using_for
-	checklist = 1..10
-	#your code here
-	for item in checklist 
-	puts "Wingardium Leviosa"
-	end
+# def using_for
+# 	checklist = 1..10
+# 	#your code here
+# 	for item in checklist 
+# 	puts "Wingardium Leviosa"
+# 	end
+# end
+
+def using_for 
+  checklist = 1..10
+  for item in checklist
+   puts "Wingardium Leviosa"
+  end
 end
-
-
